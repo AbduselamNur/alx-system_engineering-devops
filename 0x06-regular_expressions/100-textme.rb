b298d86187ec4dade3ed9cf3b152ea7bd4539954
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-08-01 18:11:33 +0300

    advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # The sender phone number or name (including country code if present)
 # The receiver phone number or name (including country code if present)
 # The flags that were used
-puts ARGV[0].scan(/\[from:(\+?\d+|[A-Za-z]+)\] \[to:(\+?\d+|[A-Za-z]+)\] \[flags:(-?\d+:-?\d+:-?\d+:-?\d+:-?\d+)\]/).join
+puts ARGV[0].scan(/(?<=from:|to:|flags:).*?(?=\])/).join(',')
